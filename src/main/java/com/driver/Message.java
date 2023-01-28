56231a51c034c5f8169e9a1d5e4ae219c3dc74f9
Shubham Khetre <shubhamek21@gmail.com>
2023-01-28 17:00:57 +0530

    First try

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -1,10 +1,40 @@
 package com.driver;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Message {
     private int id;
     private String content;
-    private Date timestamp;
+    private LocalDate timestamp;
 
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = LocalDate.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDate timestamp) {
+        this.timestamp = timestamp;
+    }
 }
